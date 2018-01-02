b2bba448302c1af8b74db3e1c1dca0215b96c728
Andrea Ravalli <anravalli@gmail.com>
2018-01-02 19:38:59 +0100

    License and Copyright statement added

--- app/src/main/java/flyOnTheWall/FlyOnTheWall.java
+++ app/src/main/java/flyOnTheWall/FlyOnTheWall.java
@@ -1,3 +1,24 @@
+/*
+ *	Fly On The Wall: a Fly survival game :-)
+ *
+ *	Copyright 2012 - 2018 Andrea Ravalli <anravalli @ gmail.com>
+ *
+ *	This file is part of Fly On The Wall.
+ *
+ *	Fly On The Wall is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+
+ *	Fly On The Wall is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+
+ *	You should have received a copy of the GNU General Public License
+ *	along with Fly On The Wall.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package flyOnTheWall;
 
 import android.app.Activity;
@@ -14,11 +35,8 @@ import flyOnTheWall.base.msg.GameMessage;
 import flyOnTheWall.base.msg.GameMessagesType;
 import flyOnTheWall.base.msg.OnNewGameMessage;
 
-//import android.widget.ProgressBar;
-//import android.widget.ToggleButton;
-
 /**
- * @author Andrea Ravalli <anravlli @ gmail.com>
+ * @author Andrea Ravalli <anravalli @ gmail.com>
  * @version 0.6
  * @since 2012
  * <p/>
